fd47456a4fd3b5ee0e3adb626d9136b5fee07cf3
Julio Cezar Pereira <juliocezarpereira@Julios-MacBook.lan>
2021-09-01 09:16:01 -0300

    Trabalho feito

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
 
+const btnMobile = document.getElementById('btn-mobile');
+
+function menuMobile(){
+    if(event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+    if (active){
+        event.currentTarget.setAttribute('aria-label', 'Fechar menu');
+    }else{
+        event.currentTarget.setAttribute('aria-label', 'Abrir menu');
+    }
+}
+btnMobile.addEventListener('click', menuMobile);
+btnMobile.addEventListener('touchstart', menuMobile);
+
+
